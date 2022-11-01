7725bc6f7f118731abdecc6c2a4d87f6e296dd42
Paulo Scapol Barbosa <paulo.scapol2@hotmail.com>
2022-11-01 18:28:00 -0300

    Parte 8 do exercício pós Aula ao vivo

--- M1_Fundamentos/S5_JavaScript:DOM_Eventos_e_WebStorage/Dia_3_JavaScript-Eventos/exercicioPosAulaAoVivo/script.js
+++ M1_Fundamentos/S5_JavaScript:DOM_Eventos_e_WebStorage/Dia_3_JavaScript-Eventos/exercicioPosAulaAoVivo/script.js
@@ -164,5 +164,25 @@ const addTarefa = (tarefa) => {
     containerTarefa.appendChild(nomeTarefa);
 }
 
-addTarefa('Projeto<br>');
-addTarefa('<br>Estudar');
+addTarefa('Projeto');
+
+// Parte 8
+// * Implemente uma função que adicione uma legenda com cor para a tarefa;
+// * Essa função deverá receber como parâmetro uma string (‘cor’) e criar dinamicamente um elemento de tag <div> 
+//   com a classe task;
+// * O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada;
+// * O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+const addLegenda = (color) => {
+    let buscarTarefas = document.querySelector('.my-tasks');
+    let novaTarefa = document.createElement('div');
+    
+    novaTarefa.className = 'task';
+    novaTarefa.style.backgroundColor = color;
+
+    buscarTarefas.appendChild(novaTarefa);
+}
+
+addLegenda('yellow');
+
+// Parte 9
+// * 
