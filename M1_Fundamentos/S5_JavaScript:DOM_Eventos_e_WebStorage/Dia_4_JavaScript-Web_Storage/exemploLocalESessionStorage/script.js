68e7118b07cbeef353f254c3d5d465821eaf0eb7
Paulo Scapol Barbosa <paulo.scapol2@hotmail.com>
2022-11-03 15:37:30 -0300

    Exercício de Exemplo

---
+++ M1_Fundamentos/S5_JavaScript:DOM_Eventos_e_WebStorage/Dia_4_JavaScript-Web_Storage/exemploLocalESessionStorage/script.js
@@ -0,0 +1,71 @@
+// Session Storage | | | |  (Perde os dados ao dar F5 na página ou fechar a aba)
+//                 V V V V
+
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+const insertPhraseInDOM = () => {
+  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
+
+const addPhraseToSessionStorage = () => {
+  if (sessionStorage.getItem('phrases') === null) {
+    sessionStorage.setItem('phrases', JSON.stringify([]));
+  }
+  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  sessionStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+}
+
+button.addEventListener('click', addPhraseToSessionStorage);
+
+
+// Local Storage | | | |  (Mantém os dados mesmo ao dar F5 na página ou fechar a aba)
+//               V V V V
+
+// const button = document.getElementById('add-button');
+// const input = document.getElementById('phrases-input');
+// const list = document.getElementById('phrases-list');
+
+// const insertPhraseInDOM = () => {
+//   const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+//   const listLength = phrasesList.length - 1;
+//   const phraseText = phrasesList[listLength];
+//   const phrase = document.createElement('li');
+//   phrase.innerText = phraseText;
+//   list.appendChild(phrase);
+// }
+
+// const addPhraseToLocalStorage = () => {
+//   const oldList = JSON.parse(localStorage.getItem('phrases'));
+//   const phraseText = input.value;
+//   oldList.push(phraseText);
+//   localStorage.setItem('phrases', JSON.stringify(oldList));
+//   insertPhraseInDOM();
+// }
+
+// const initialRendering = () => {
+//   if (localStorage.getItem('phrases') === null) {
+//     localStorage.setItem('phrases', JSON.stringify([]));
+//   } else {
+//     const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+//     const listLength = phrasesList.length - 1;
+//     for (let index = 0; index <= listLength; index += 1) {
+//       const listElement = document.createElement('li');
+//       listElement.innerText = phrasesList[index];
+//       list.appendChild(listElement);
+//     }
+//   }
+// }
+
+// button.addEventListener('click', addPhraseToLocalStorage);
+
+// window.onload = () => initialRendering();
